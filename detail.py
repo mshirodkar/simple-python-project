222366bdd4b2769e0cfda97c85bc89560262c2db
Mahesh Shirodkar <mahesh.mshirodkar@gmail.com>
2024-11-26 20:45:11 +0530

    your details

---
+++ detail.py
@@ -0,0 +1,7 @@
+#detail.py
+class Adress:
+    def _init_(self, name, street, city, state):
+        self.name = name
+        self.street = street
+        self.city = city
+        self.state = state
